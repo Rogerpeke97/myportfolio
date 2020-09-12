fc92fa44c7e5a1e531e03f06132c13995e1ee014
Ignacio <rogerpeke97@gmail.com>
2020-09-12 18:03:38 -0300

    Updates

--- precache-manifest.adc720c9944f71a9888f7e203fd5b882.js
+++ precache-manifest.adc720c9944f71a9888f7e203fd5b882.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c52a019cd5969c56d5d16d47540bf69",
+    "revision": "4848ef7a42f8dcdfaf498515bf831815",
     "url": "/portfolio/index.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e78825765ecb24147b82daaf455703a1.js"
+  "/portfolio/precache-manifest.adc720c9944f71a9888f7e203fd5b882.js"
 );
 
 self.addEventListener('message', (event) => {
